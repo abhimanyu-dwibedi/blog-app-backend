68bbe4097490112903ec3d3bf63283499048c49b
abhimanyu <abhimanyu.dwibedi@gmail.com>
2023-04-30 13:39:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
+    "mongodb+srv://abhimanyudwibedi:abhimanyudwibedi@cluster0.fjgcqkl.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() => console.log("Connected to db and backend up"))
+  .catch((err) => console.log(err));
